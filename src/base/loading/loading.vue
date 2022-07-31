7fe69fa94ff29d803c7741b99e619545557f65dd
Agility6 <309645035@qq.com>
2022-07-31 21:46:44 +0800

    feat:basic loading

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="loading">
+    <img
+      width="24" 
+      height="24"
+      src="./loading.gif">
+      <p class="desc">{{ title }}</p>
+   </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 可传值
+    title: {
+      type: String,
+      default: '正在加载...'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import ('~@/common/stylus/variable');
+
+.loading
+  width: 100%
+  text-align: center
+  .desc
+    line-height: 20px
+    font-size: $font-size-small
+    color: $color-text-l
+</style>
